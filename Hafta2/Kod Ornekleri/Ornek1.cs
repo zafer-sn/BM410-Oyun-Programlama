96b3cfac950c1a5f567b8f7a3e7bec83a6d59679
zafer-sn <zafer.serinn@outlook.com>
2024-10-08 12:09:18 +0300

    h2

---
+++ Hafta2/Kod Ornekleri/Ornek1.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Ornek1 : MonoBehaviour
+{
+    private void Awake(){   
+    }
+    private void Start(){        
+    }
+    private void Update(){        
+    }
+    private void FixedUpdate(){        
+    }
+    private void LateUpdate(){
+        
+    }
+}
